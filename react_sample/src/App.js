f5c2146dc509ef3903c6731f56edb273405581ec
shashanka <shashanka.nshetty@latentview.com>
2025-03-13 13:11:55 +0530

    update

--- react_sample/src/App.js
+++ react_sample/src/App.js
@@ -1,21 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as RB from "react-bootstrap"
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+  let oncli_event = () => {
+    console.log("CLICK IS TRIGGERE working main");
+  };
 
-  let oncli_event=()=>{
-    console.log("CLICK IS TRIGGERE working main")
-  }
+  let return_the_text = () => {
+    return "Hello from returnfwefhwe";
+  };
 
-  let return_the_text =()=>{
-    return "Hello from returnfwefhwe"
-  }
-
-  let a =return_the_text()
+  let a = return_the_text();
 
   return (
     <div className="App" onClick={oncli_event}>
-      {a}
+      <div>
+        <RB.Button variant="success"> HELLO</RB.Button>
+        <RB.Card style={{ width: '18rem' }}>
+      <RB.Card.Img variant="top" src="holder.js/100px180" />
+      <RB.Card.Body>
+        <RB.Card.Title>Card Title</RB.Card.Title>
+        <RB.Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </RB.Card.Text>
+        <RB.Button variant="primary">Go somewhere</RB.Button>
+      </RB.Card.Body>
+    </RB.Card>
+      </div>
+      <div> {a}</div>
     </div>
   );
 }
