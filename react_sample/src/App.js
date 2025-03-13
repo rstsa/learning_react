4f80fbfd4d6798f4a247e8b5aeb9fe5bd6022aaf
shashanka <shashanka.nshetty@latentview.com>
2025-03-13 12:41:14 +0530

    initial commit

---
+++ react_sample/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let oncli_event=()=>{
+    console.log("CLICK IS TRIGGERE")
+  }
+
+  let return_the_text =()=>{
+    return "Hello from returnfwefhwe"
+  }
+
+  let a =return_the_text()
+
+  return (
+    <div className="App" onClick={oncli_event}>
+      {a}
+    </div>
+  );
+}
+
+export default App;
